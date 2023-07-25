ce9dbe5682ee1d35e7e3d8d89d9cf98e48f755af
srikanth141199 <kolleparasrikanth@gmailcom>
2023-07-25 18:21:46 +0530

    about.js

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ function About() {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">read more</a>
+          <a href="#home" className="btn">read more</a>
         </article>
       </div>
     </section>
